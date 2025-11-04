cf506ad6bdc3d41f5011a09640b145a668d216c0
Xiwei Lim <xiwlim@u81z04s04.42kl.edu.my>
2025-11-04 14:14:22 +0800

    update

--- core/libft/ft_memcmp.c
+++ core/libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: xiwlim <xiwlim@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 13:11:14 by xiwlim            #+#    #+#             */
-/*   Updated: 2025/10/27 15:30:56 by xiwlim           ###   ########.fr       */
+/*   Updated: 2025/11/04 11:40:52 by xiwlim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,18 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*str;
-	const unsigned char	*str2;
-	unsigned int		i;
+	unsigned char	*str;
+	unsigned char	*str2;
 
-	if (n == 0)
-		return (0);
-	i = 0;
-	str = (const unsigned char *)s1;
-	str2 = (const unsigned char *)s2;
-	while (str[i] && str[i] == str2[i] && i < n - 1)
-		i++;
-	return (str[i] - str2[i]);
+	str = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	while (n != 0)
+	{
+		if (*str != *str2)
+			return (*str - *str2);
+		str++;
+		str2++;
+		n--;
+	}
+	return (0);
 }
